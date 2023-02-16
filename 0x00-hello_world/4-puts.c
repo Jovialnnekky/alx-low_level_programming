38b45d910dfeb597f6456959a9292e34abc12176
jovialnnekky <nnwokoro15@gmail.com>
2023-02-16 06:48:31 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include stdio.h
 /**
  * main - Entry point
- * Return: o (sucess)
+ * Return: 0 (sucess)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (o);
+return (0);
 }
