14d4ffeefc9bf8c400cdcf9002e1b2ecde87cdc3
jovialnnekky <nnwokoro15@gmail.com>
2023-02-17 05:23:23 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	fput("and that piece of act is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write("2, and that piece of act is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
