d7ca146e5e6a011a5771105a7c84abf7e606e0d6
jovialnnekky <nnwokoro15@gmail.com>
2023-02-17 05:10:54 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	fput("and that piece of act is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
